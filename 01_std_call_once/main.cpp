6a23eb12bfe51ba1086ab18475935e2aeade72d4
yanqing <252487266@qq.com>
2022-08-02 14:30:29 +0800

    std::call_once

---
+++ 01_std_call_once/main.cpp
@@ -0,0 +1,40 @@
+﻿#include <QCoreApplication>
+#include <QDebug>
+#include <thread>
+#include <mutex>
+class Resource
+{
+public:
+    Resource() {}
+    int a = 0;
+    void do_something() {
+        qDebug()<< this->a;
+    }
+};
+std::shared_ptr<Resource> resource_ptr;
+std::once_flag resource_flag;
+
+void init_resource() {
+    resource_ptr.reset(new Resource);
+    resource_ptr->a = resource_ptr->a + 1;
+}
+
+void foo() {
+    //可以完成的进行一次初始化
+    std::call_once(resource_flag,init_resource);
+    resource_ptr->do_something ();
+}
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    for (int i = 0; i < 10; ++i) {
+        std::thread([=]() {
+            foo();
+        }).detach();
+    }
+
+/*打印结果全为1,可知只执行了一次 初始化*/
+
+    return a.exec();
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Multithread-for-C-Plus
 c++多线程
+
+[01 std::once() 多线程操作,保证资源初始化执行一次](./01_std_call_once/main.cpp)
